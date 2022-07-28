cc237fee848aad2c577aa8d65ff39c9e3f59922b
Gustavo Kurek <gustavokureksz@gmail.com>
2022-07-27 21:43:01 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+app.get('/', (req, res) => {
+  return res.json('funfonando');
+});
+
+app.listen(3333, () => {
+  console.log('http://localhost:3333/');
+});

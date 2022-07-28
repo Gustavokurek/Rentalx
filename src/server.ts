b99aa41346ea2cd8d41ad69c654b4ee82f0ac4d5
Gustavo Kurek <gustavokureksz@gmail.com>
2022-07-27 23:29:42 -0300

    configurando debug

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 app.get('/', (req, res) => {
   return res.json('funfonando');
 });
 
+app.post('/courses', (req, res) => {
+  const { name } = req.headers;
+  return res.json({ name });
+});
 app.listen(3333, () => {
   console.log('http://localhost:3333/');
 });

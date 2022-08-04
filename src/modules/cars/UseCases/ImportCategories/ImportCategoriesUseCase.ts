696de4ef7d9eabff208ab46c88a69f377733eea5
Gustavo Kurek <gustavokureksz@gmail.com>
2022-08-04 18:13:23 -0300

    leitura em stream e csvParser

--- src/modules/cars/UseCases/ImportCategories/ImportCategoriesUseCase.ts
+++ src/modules/cars/UseCases/ImportCategories/ImportCategoriesUseCase.ts
@@ -1,5 +1,15 @@
+import { Parser } from 'csv-parse';
+import fs from 'fs';
+
 export class ImportCategoriesUseCase {
-  execute(file: unknown): void {
-    console.log(file);
+  execute(file: Express.Multer.File): void {
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = new Parser({});
+    stream.pipe(parseFile);
+
+    parseFile.on('data', (line) => {
+      console.log(line);
+    });
   }
 }

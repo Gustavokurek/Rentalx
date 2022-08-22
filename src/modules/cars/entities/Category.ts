59ef55566ffefe481e1624cacc01a7d55a0c0be4
Gustavo Kurek <gustavokureksz@gmail.com>
2022-08-22 18:55:07 -0300

    refactory model categories

--- src/modules/cars/entities/Category.ts
+++ src/modules/cars/entities/Category.ts
@@ -1,9 +1,15 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('categories')
 export class Category {
+  @PrimaryColumn()
   id?: string;
+  @Column()
   nameCategory: string;
+  @Column()
   description: string;
+  @CreateDateColumn()
   created_at: Date;
   constructor() {
     if (!this.id) {

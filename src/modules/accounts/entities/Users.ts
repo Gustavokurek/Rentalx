58f2dc855923ac2d52e109aab2e6329786746d93
Gustavo Kurek <gustavokureksz@gmail.com>
2022-08-28 20:05:39 -0300

    create migration and entity of Users

---
+++ src/modules/accounts/entities/Users.ts
@@ -0,0 +1,28 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
+import { v4 } from 'uuid';
+
+@Entity('users')
+export class Users {
+  @PrimaryColumn()
+  id: string;
+  @Column()
+  name: string;
+  @Column()
+  username: string;
+  @Column()
+  password: string;
+  @Column()
+  email: string;
+  @Column()
+  driver_license: string;
+  @Column()
+  admin: boolean;
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = v4();
+    }
+  }
+}
